68720d1fb3ff8129f68c5548d3cc40bb0121770a
Wojciech Gawronski <afronski@gmail.com>
2018-07-17 11:43:59 +0200

    Final fixes (iframe always works).

--- src/components/map.js
+++ src/components/map.js
@@ -2,31 +2,29 @@ import React from "react";
 
 import styled from "styled-components";
 
-import { Map, Marker, Popup, TileLayer } from "react-leaflet";
+import { rhythm } from "../theme/typography";
 
-import "leaflet/dist/leaflet.css";
+const MapComponent = styled.iframe`
+  margin-bottom: 0;
+`;
 
-const StyledMap = styled(Map)`
-  height: 30vw;
+const MapDescription = styled.p`
+  margin-bottom: ${rhythm(1)};
 `;
 
 export const MapWithMarker = ({ location, name, link }) => {
-  const position = [
-    location.lat,
-    location.lng
-  ];
+  const url = `https://www.openstreetmap.org/?mlat=${location.lat}&amp;mlon=${location.lng}#map=19/${location.lat}/${location.lng}`;
 
   return (
-    <StyledMap center={position} zoom={17}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-      />
-      <Marker position={position}>
-        <Popup>
-          <a href={link} target="_blank">{name}</a>
-        </Popup>
-      </Marker>
-    </StyledMap>
+    <div>
+      <div>
+        <MapComponent width="300" height="350" frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0" src={link} style={{ border: `1px solid black` }}></MapComponent>
+      </div>
+      <MapDescription>
+        <a href={url} target="_blank">
+          {name}
+        </a>
+      </MapDescription>
+    </div>
   );
 }
